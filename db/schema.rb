5aa613e828820c308cfba9b2071d3e8dcda779d4
ethornbury <ethornbury@eircom.net>
2015-06-04 07:56:35 +0000

    admin authentication working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603150437) do
+ActiveRecord::Schema.define(version: 20150604074425) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20150603150437) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "admin",                  default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
