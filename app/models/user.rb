a6666b8239d7b50f28fa0d650e4c7e453f0784db
ethornbury <ethornbury@eircom.net>
2015-06-04 11:48:51 +0000

    change password length from 8 to 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :timeoutable, password_length: 6..128
          
   def timeout_in
     if self.admin? 
