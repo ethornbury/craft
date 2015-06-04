a6469bb5d8488307601f1706a3176f9cc7e72e1a
ethornbury <ethornbury@eircom.net>
2015-06-04 11:37:33 +0000

    front page style

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+         
+  def timeout_in
+    if self.admin? 
+      2.hours
+    else
+      1.hour
+    end
+  end       
 end
